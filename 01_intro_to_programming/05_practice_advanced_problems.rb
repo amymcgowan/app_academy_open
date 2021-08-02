df569904f17167c97145efb8b4aa9ed26259006b
amymcgowan <amymcgowan624@gmail.com>
2021-08-02 16:01:21 -0400

    enumerate file names

---
+++ 01_intro_to_programming/05_practice_advanced_problems.rb
@@ -0,0 +1,91 @@
+# def map_by_name
+
+# end
+
+# def map_by_key
+
+# end
+
+# def yell_sentence
+
+# end
+
+# def whisper_words
+
+# end
+
+# def o_words
+
+# end
+
+# def last_index
+
+# end
+
+# def most_vowels
+
+# end
+
+# def prime
+
+# end
+
+# def pick_primes
+
+# end
+
+# def greatest_factor_array
+
+# end
+
+# def perfect_square
+
+# end
+
+# def triple_sentence
+
+# end
+
+# def summation_sequence
+
+# end
+
+# def fibonacci
+
+# end
+
+# def caesar_cipher
+
+# end
+
+# def vowel_cipher
+
+# end
+
+# def double_letter_count
+
+# end
+
+# def adjacent_sum
+
+# end
+
+# def pyramid_sum
+
+# end
+
+# def all_else_equal
+
+# end
+
+# def anagrams
+
+# end
+
+# def consonant_cancel
+
+# end
+
+# def same_char_collapse
+
+# end
