8e220ebd4929eb309353b6e1690a4e28b045e959
amymcgowan <amymcgowan624@gmail.com>
2021-08-02 20:14:52 -0400

    add advanced

--- 01_intro_to_programming/05_practice_advanced_problems.rb
+++ 01_intro_to_programming/05_practice_advanced_problems.rb
@@ -1,14 +1,48 @@
-# def map_by_name
+# def map_by_name(arr)
+#     arr.map { |hash| hash["name"] }
+# end
+
+# pets = [
+#   {"type"=>"dog", "name"=>"Rolo"},
+#   {"type"=>"cat", "name"=>"Sunny"},
+#   {"type"=>"rat", "name"=>"Saki"},
+#   {"type"=>"dog", "name"=>"Finn"},
+#   {"type"=>"cat", "name"=>"Buffy"}
+# ]
+# print map_by_name(pets) #=> ["Rolo", "Sunny", "Saki", "Finn", "Buffy"]
+# puts
+
+# countries = [
+#  {"name"=>"Japan", "continent"=>"Asia"},
+#  {"name"=>"Hungary", "continent"=>"Europe"},
+#  {"name"=>"Kenya", "continent"=>"Africa"},
+# ]
+# print map_by_name(countries) #=> ["Japan", "Hungary", "Kenya"]
+# puts
+
+# def map_by_key(arr, key_str)
+
+#     arr.map { |hash| hash[key_str] }
 
 # end
 
-# def map_by_key
+# locations = [
+#   {"city"=>"New York City", "state"=>"New York", "coast"=>"East"},
+#   {"city"=>"San Francisco", "state"=>"California", "coast"=>"West"},
+#   {"city"=>"Portland", "state"=>"Oregon", "coast"=>"West"},
+# ]
 
+# print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
+# puts
+# print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
+# puts
+
+# def yell_sentence(sent)
+
+#     sent.split(" ").map { |word| word.upcase + "!" }.join(" ")
 # end
 
-# def yell_sentence
-
-# end
+# puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
 
 # def whisper_words
 
@@ -62,9 +96,16 @@
 
 # end
 
-# def double_letter_count
-
+# def double_letter_count(str)
+#     count = 0
+#     str.each_char.with_index do |char, i|
+#         count +=1 if char == str[i + 1]
+#     end
+#     count
 # end
+
+# puts double_letter_count("the jeep rolled down the hill") #=> 3
+# puts double_letter_count("bootcamp") #=> 1
 
 # def adjacent_sum
 
@@ -82,10 +123,47 @@
 
 # end
 
-# def consonant_cancel
-
+# def consonant_cancel(sent)
+#     words = sent.split
+#     new_words = words.map { |word| remove_first_consonant(word) }
+#     new_words.join(" ")
 # end
 
-# def same_char_collapse
-
+# def remove_first_consonant(word)
+#     vowels = "aeiou"
+#     word.each_char.with_index do |char, i|
+#         return word[i..-1] if vowels.include?(char)
+#     end
 # end
+
+# puts consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
+# puts consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
+
+# def same_char_collapse(str)
+#     continue = true
+
+#     while continue
+#         chars = str.split("")
+#         continue = false
+
+#         chars.each.with_index do |char, i|
+#             if chars[i] == chars[i + 1]
+#                 chars[i] = ""
+#                 chars[i + 1] = ""
+#                 continue = true
+#             end
+#         end
+
+#         new_str = chars.join("")
+#     end
+#     new_str
+# end
+
+# puts same_char_collapse("zzzxaaxy")   #=> "zy"
+# # because zzzxaaxy -> zxaaxy -> zxxy -> zy
+
+
+# puts same_char_collapse("uqrssrqvtt") #=> "uv"
+# # because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
+
+# ???
